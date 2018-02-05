59aef858d0757b88b81b4f4df10a08503fceb454
pdgsol <pdgomezsol@gmail.com>
2018-02-05 09:11:41 +0100

    Updatig layout

--- app/src/main/java/com/example/pdg/calulatorapi22/CalculatorActivity.java
+++ app/src/main/java/com/example/pdg/calulatorapi22/CalculatorActivity.java
@@ -95,8 +95,6 @@ public class CalculatorActivity  extends AppCompatActivity implements View.OnCli
                     startNewScreen = true;
                     e.printStackTrace();
                 }
-                //Toast.makeText(getApplicationContext(),"Calculando...",Toast.LENGTH_SHORT).show();
-
             }
         });
 
