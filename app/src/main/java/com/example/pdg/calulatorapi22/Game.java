0307430dafab75bc3c721420373c86dba8c0be5a
pdgsol <pdgomezsol@gmail.com>
2018-02-06 09:29:03 +0100

    Adding game

--- app/src/main/java/com/example/pdg/calulatorapi22/Game.java
+++ app/src/main/java/com/example/pdg/calulatorapi22/Game.java
@@ -6,14 +6,23 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageView;
 
 public class Game extends AppCompatActivity {
 
+    ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
-      
+        imageView= (ImageView) findViewById(R.id.card11);
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                imageView.setImageResource(R.drawable.images);
+            }
+        });
     }
 
 }
